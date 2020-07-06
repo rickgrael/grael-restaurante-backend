00154f9f8c3440b716d5c63ea47c15cadf54471d
rickgrael <ricardochvs23@gmail.com>
2020-07-05 21:55:41 -0300

    adicionando credenciais de cleardb

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,9 +1,8 @@
 const sequelize = require('sequelize')
 
-const connection = new sequelize('grael_fluxo','root','MRpoladoful1998',{
-    host: 'localhost',
+const connection = new sequelize('heroku_fdd73bddf1ae39c','b7373e6a352a83','678b8460',{
+    host: 'us-cdbr-east-02.cleardb.com',
     dialect: 'mysql'
 });
-
 
 module.exports = connection;
