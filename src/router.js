6970f240fd5a5ef35686307326654ac43d45a959
rickgrael <ricardochvs23@gmail.com>
2020-08-15 18:22:46 -0300

    adicionado rota de edicao de vendas

--- src/router.js
+++ src/router.js
@@ -7,42 +7,68 @@ const individualController = require('./controllers/individualController');
 const Compras = require('./database/models/Compras');
 const Vendas = require('./database/models/Vendas');
 
-router.get('/api/compras', async (req,res)=>{
+router.get('/api/compras', async (req, res) => {
     const compras = await Compras.findAll();
     res.json(compras);
 })
 
-router.get('/api/vendas', async (req,res)=>{
+router.get('/api/vendas', async (req, res) => {
     const vendas = await Vendas.findAll();
     res.json(vendas);
 })
 
-router.post('/api/compras/new/NFe',fileControllers.getXmlFile);
+router.post('/api/compras/new/NFe', fileControllers.getXmlFile);
 
-router.post('/api/compras/new/NFCe',fileControllers.getHtmlFile);
+router.post('/api/compras/new/NFCe', fileControllers.getHtmlFile);
 
-router.post('/api/vendas/new/NFCe',fileControllers.getHtmlFile);
+router.post('/api/vendas/new/NFCe', fileControllers.getHtmlFile);
 
-router.post('/api/vendas/new/NFe',fileControllers.getXmlFile);  
+router.post('/api/vendas/new/NFe', fileControllers.getXmlFile);
 
-router.post('/api/vendas/new/individual',individualController.individualVenda);
+router.post('/api/vendas/new/individual', individualController.individualVenda);
 
-router.post('/api/compras/new/individual',individualController.individualCompra);
+router.post('/api/compras/new/individual', individualController.individualCompra);
 
-router.post('/api/auth', async (req,res)=>{
+router.post('/api/auth', async (req, res) => {
     const {
         user,
         password
     } = req.body;
     console.log(user, password)
-    if( user == 'grael' && password == 'restaurante'){
+    if (user == 'grael' && password == 'restaurante') {
         const token = new Date();
         token.setDate(token.getDate() + 1);
         res.send(token);
-    }else{
+    } else {
         res.send("falha")
     }
 
+})
+
+router.post('/editar/venda', async (req, res) => {
+    const {
+        id,
+        nomeProduto: nome_produto,
+        quantidade,
+        valorUnidade: valor_unitario,
+        valorTotal: valor_total,
+        dataEmissao: data_emissao
+    } = req.body;
+
+    await Vendas.update({
+        id,
+        nome_produto,
+        quantidade,
+        valor_unitario,
+        valor_total,
+        data_emissao
+    }, {
+        where: {
+            id
+        }
+    })
+
+    res.redirect('http://localhost:3000/entradas')
 })
 
 module.exports = router;
